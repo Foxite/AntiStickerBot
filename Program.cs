de216f29d0a5bbf4c405df9e903a24da611ca6b5
Dirk Kok <the@dirkkok.nl>
2022-01-11 21:19:05 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DSharpPlus;
+
+var discord = new DiscordClient(new DiscordConfiguration() {
+	Token = Environment.GetEnvironmentVariable("BOT_TOKEN")
+});
+
+discord.MessageCreated += async (_, eventArgs) => {
+	if (eventArgs.Message.Stickers.Count > 0) {
+		await eventArgs.Message.DeleteAsync();
+	}
+};
+
+await discord.ConnectAsync();
+
+await Task.Delay(-1);
