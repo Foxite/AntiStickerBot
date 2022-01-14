ddcb911ca2a7398013ea6139cb2838f0f61a1a50
Dirk Kok <the@dirkkok.nl>
2022-01-14 17:40:51 +0100

    ignored channels

--- Program.cs
+++ Program.cs
@@ -4,8 +4,11 @@ var discord = new DiscordClient(new DiscordConfiguration() {
 	Token = Environment.GetEnvironmentVariable("BOT_TOKEN")
 });
 
+string? envVarIgnoredChannels = Environment.GetEnvironmentVariable("BOT_TOKEN");
+ulong[] ignoredChannels = envVarIgnoredChannels == null ? Array.Empty<ulong>() : envVarIgnoredChannels.Split(";").Select(ulong.Parse).ToArray();
+
 discord.MessageCreated += async (_, eventArgs) => {
-	if (eventArgs.Message.Stickers.Count > 0) {
+	if (eventArgs.Message.Stickers.Count > 0 && !ignoredChannels.Contains(eventArgs.Channel.Id)) {
 		await eventArgs.Message.DeleteAsync();
 	}
 };
