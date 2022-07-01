3ed03d7db9c2283576da36f4a10e8d9ac1dd3b4e
Dirk Kok <the@dirkkok.nl>
2022-07-01 21:44:12 +0200

    split bots

---
+++ AntiEmoteBot/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using AntiStickerBot;
+using DSharpPlus;
+using DSharpPlus.Entities;
+
+[return: NotNullIfNotNull("defaultValue")]
+T ParseEnvvarOrDefault<T>(string name, T defaultValue, Func<string, T> parse) {
+	string? envvar = Environment.GetEnvironmentVariable(name);
+	if (envvar != null) {
+		return parse(envvar);
+	} else {
+		return defaultValue;
+	}
+}
+
+int maxEmotes = ParseEnvvarOrDefault("MAX_EMOTES", 0, int.Parse);
+ulong[] ignoredRoles = ParseEnvvarOrDefault("EMOTE_IGNORED_ROLES", Array.Empty<ulong>(), envvar => envvar.Split(";").Select(ulong.Parse).ToArray());
+
+var discord = new DiscordClient(new DiscordConfiguration() {
+	Token = Environment.GetEnvironmentVariable("BOT_TOKEN"),
+});
+
+var emoteRegex = new Regex(@"(<a?)?:\w+:(\d{18}>)?");
+
+IEnumerable<string> emojis = ((IReadOnlyDictionary<string, string>) typeof(DiscordEmoji).GetProperty("DiscordNameLookup", BindingFlags.NonPublic | BindingFlags.Static)!.GetValue(null)!).Keys;
+var emojiTree = new StringTree();
+foreach (string emoji in emojis) {
+	emojiTree.Add(emoji);
+}
+
+Task FilterEmojis(DiscordMessage message) {
+	if (message.Author is DiscordMember author && author.Roles.Select(role => role.Id).Intersect(ignoredRoles).Any()) {
+		return Task.CompletedTask;
+	}
+
+	ReadOnlySpan<char> span = message.Content.AsSpan();
+	int count = emoteRegex.Matches(message.Content).Count;
+	if (count > maxEmotes) {
+		return message.DeleteAsync();
+	}
+
+	for (int i = 0; i < message.Content.Length;) {
+		int length = emojiTree.ContainsPrefix(span[i..]);
+		if (length != -1) {
+			i += length;
+			count++;
+
+			if (count > maxEmotes) {
+				return message.DeleteAsync();
+			}
+		} else {
+			i++;
+		}
+	}
+
+	return Task.CompletedTask;
+}
+
+discord.MessageCreated += (_, ea) => FilterEmojis(ea.Message);
+discord.MessageUpdated += (_, ea) => FilterEmojis(ea.Message);
+
+await discord.ConnectAsync();
+
+await Task.Delay(-1);
--- AntiStickerBot/Program.cs
+++ AntiStickerBot/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
-using System.Text.RegularExpressions;
-using AntiStickerBot;
 using DSharpPlus;
 using DSharpPlus.Entities;
-using DSharpPlus.EventArgs;
 
 [return: NotNullIfNotNull("defaultValue")]
 T ParseEnvvarOrDefault<T>(string name, T defaultValue, Func<string, T> parse) {
@@ -16,73 +12,19 @@ T ParseEnvvarOrDefault<T>(string name, T defaultValue, Func<string, T> parse) {
 	}
 }
 
-bool deleteStickers            = ParseEnvvarOrDefault("DELETE_STICKERS",          true,                 bool.Parse);
-int maxEmotes                  = ParseEnvvarOrDefault("MAX_EMOTES",               0,                    int.Parse);
-ulong[] stickerIgnoredRoles    = ParseEnvvarOrDefault("STICKER_IGNORED_ROLES",    Array.Empty<ulong>(), envvar => envvar.Split(";").Select(ulong.Parse).ToArray());
-ulong[] stickerIgnoredChannels = ParseEnvvarOrDefault("STICKER_IGNORED_CHANNELS", Array.Empty<ulong>(), envvar => envvar.Split(";").Select(ulong.Parse).ToArray());
-ulong[] emoteIgnoredRoles      = ParseEnvvarOrDefault("EMOTE_IGNORED_ROLES",      Array.Empty<ulong>(), envvar => envvar.Split(";").Select(ulong.Parse).ToArray());
-ulong[] emoteIgnoredChannels   = ParseEnvvarOrDefault("EMOTE_IGNORED_CHANNELS",   Array.Empty<ulong>(), envvar => envvar.Split(";").Select(ulong.Parse).ToArray());
-
-bool IsIgnored(DiscordUser user, DiscordChannel channel, bool emotes) {
-	ulong[] ignoredChannels = emotes ? emoteIgnoredChannels : stickerIgnoredChannels;
-	ulong[] ignoredRoles = emotes ? emoteIgnoredRoles : stickerIgnoredRoles;
-	return user is not DiscordMember member || ignoredChannels.Contains(channel.Id) || member.Roles.Select(role => role.Id).Intersect(ignoredRoles).Any();
-}
+ulong[] ignoredRoles = ParseEnvvarOrDefault("STICKER_IGNORED_ROLES", Array.Empty<ulong>(), envvar => envvar.Split(";").Select(ulong.Parse).ToArray());
 
 var discord = new DiscordClient(new DiscordConfiguration() {
 	Token = Environment.GetEnvironmentVariable("BOT_TOKEN"),
 });
 
-if (deleteStickers) {
-	discord.MessageCreated += (_, ea) => {
-		if (!IsIgnored(ea.Author, ea.Channel, false) && ea.Message.Stickers.Count > 0) {
-			return ea.Message.DeleteAsync();
-		} else {
-			return Task.CompletedTask;
-		}
-	};
-}
-
-if (maxEmotes > 0) {
-	var emoteRegex = new Regex(@"(<a?)?:\w+:(\d{18}>)?");
-	
-	IEnumerable<string> emojis = ((IReadOnlyDictionary<string, string>) typeof(DiscordEmoji).GetProperty("DiscordNameLookup", BindingFlags.NonPublic | BindingFlags.Static)!.GetValue(null)!).Keys;
-	var emojiTree = new StringTree();
-	foreach (string emoji in emojis) {
-		emojiTree.Add(emoji);
-	}
-
-	Task FilterEmojis(DiscordMessage message) {
-		if (IsIgnored(message.Author, message.Channel, true)) {
-			return Task.CompletedTask;
-		}
-
-		ReadOnlySpan<char> span = message.Content.AsSpan();
-		int count = emoteRegex.Matches(message.Content).Count;
-		if (count > maxEmotes) {
-			return message.DeleteAsync();
-		}
-
-		for (int i = 0; i < message.Content.Length;) {
-			int length = emojiTree.ContainsPrefix(span[i..]);
-			if (length != -1) {
-				i += length;
-				count++;
-
-				if (count > maxEmotes) {
-					return message.DeleteAsync();
-				}
-			} else {
-				i++;
-			}
-		}
-
+discord.MessageCreated += (_, ea) => {
+	if (!(ea.Author is DiscordMember user && user.Roles.Select(role => role.Id).Intersect(ignoredRoles).Any()) && ea.Message.Stickers.Count > 0) {
+		return ea.Message.DeleteAsync();
+	} else {
 		return Task.CompletedTask;
 	}
-
-	discord.MessageCreated += (_, ea) => FilterEmojis(ea.Message);
-	discord.MessageUpdated += (_, ea) => FilterEmojis(ea.Message);
-}
+};
 
 await discord.ConnectAsync();
 
